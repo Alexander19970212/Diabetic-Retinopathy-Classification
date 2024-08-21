6aa689865fc715b4dbcfe38ff5c4d4a71f1d52f2
Alexander19970212 <kalexu97@mail.ru>
2024-08-21 12:18:25 +0300

    [FIXED] loading weights

--- model/SSIT/ssit_models.py
+++ model/SSIT/ssit_models.py
@@ -5,10 +5,21 @@ from vits import archs, resize_pos_embed
 
 def load_checkpoint(model, checkpoint_path, checkpoint_key, linear_key):
     checkpoint = torch.load(checkpoint_path)
-    # print(checkpoint)
-    # state_dict = checkpoint['state_dict']
-    state_dict = checkpoint.state_dict
-    print(state_dict)
+
+    # replace all torch-10 GELU's by torch-12 GELU
+    
+    # def torchmodify(name) :
+    #     a=name.split('.')
+    #     for i,s in enumerate(a) :
+    #         if s.isnumeric() :
+    #             a[i]="_modules['"+s+"']"
+    #     return '.'.join(a)
+    # import torch.nn as nn
+    # for name, module in checkpoint.named_modules() :
+    #     if isinstance(module,nn.GELU) :
+    #         exec('checkpoint.'+torchmodify(name)+'=nn.GELU()')
+
+    state_dict = checkpoint.state_dict()
     for k in list(state_dict.keys()):
         # retain only base_encoder up to before the embedding layer
         if k.startswith(checkpoint_key) and not k.startswith('%s.%s' % (checkpoint_key, linear_key)):
@@ -39,10 +50,10 @@ class SSitEncoder(nn.Module):
         linear_key = 'head'
         checkpoint_key = 'base_encoder'
         
-        # if checkpoint:
-        #     load_checkpoint(self.model, checkpoint, checkpoint_key, linear_key)
-        # else:
-        #     print('No checkpoint provided. Training from scratch.')
+        if checkpoint:
+            load_checkpoint(self.model, checkpoint, checkpoint_key, linear_key)
+        else:
+            print('No checkpoint provided. Training from scratch.')
 
     def forward(self, pixel_values):
         # define function in transformers library maner
